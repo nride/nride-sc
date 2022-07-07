fd9f2cf731f08a2c7c10887e37748e29560446e3
martin arrivets <arrivets@gmail.com>
2022-07-07 15:30:31 +0100

    Add bob to balances script

--- scripts/token-balance.sh
+++ scripts/token-balance.sh
@@ -9,3 +9,7 @@ echo ''
 echo 'alice:'
 junod query wasm contract-state smart juno14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9skjuwg8 \
 '{"balance":{"address":"'"$(junod keys show -a alice)"'"}}'
+
+echo 'bob:'
+junod query wasm contract-state smart juno14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9skjuwg8 \
+'{"balance":{"address":"'"$(junod keys show -a bob)"'"}}'
