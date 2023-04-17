13b20b6e7b10a2b6bac26e5603a7f0e897cf6291
martin arrivets <arrivets@gmail.com>
2023-04-17 16:17:07 +0100

    Set  admin in registry instantiation

--- scripts/registry/init-registry.sh
+++ scripts/registry/init-registry.sh
@@ -7,7 +7,7 @@ CODE=$1
 $CMD tx wasm instantiate $CODE \
     '{}' \
     --label "NRIDE REGISTRY INIT" \
-    --no-admin \
+    --admin $($CMD keys show -a faucet) \
     --from faucet \
     --chain-id $CHAINID \
     --gas-prices 0.1$FEETOKEN \
