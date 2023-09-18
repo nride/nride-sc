1042411ee5c4f4fc1405527257d07590df420605
martin arrivets <arrivets@gmail.com>
2023-09-18 10:44:38 +0100

    Add msg.rs

---
+++ cw-nride-escrow-v2/src/msg.rs
@@ -0,0 +1,64 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cw20::{ Cw20ReceiveMsg, Balance};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    /// This accepts a properly-encoded ReceiveMsg from a cw20 contract
+    /// to create escrows
+    Receive(Cw20ReceiveMsg),
+    /// Withdraw can be sent directly to the escrow contract
+    Withdraw(WithdrawMsg),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ReceiveMsg {
+    Create(CreateMsg),
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct CreateMsg {
+    /// id is a human-readable name for the escrow to use later
+    /// 3-20 bytes of utf-8 text
+    pub id: String,
+    pub user_b: String,
+    pub lock: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct WithdrawMsg {
+    pub id: String,
+    pub secret: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    /// Show all open escrows. Return type is ListResponse.
+    List {},
+    /// Returns the details of the named escrow, error if not created
+    /// Return type: DetailsResponse.
+    Details { id: String },
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct ListResponse {
+    /// list all registered ids
+    pub escrows: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
+pub struct DetailsResponse {
+    /// id of this escrow
+    pub id: String,
+    pub user_a: String,
+    pub user_b: String,
+    pub deposit: Balance,
+    pub lock: Option<String>,
+}
